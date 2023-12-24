8655939c10ff44b9c8c1212731494d1e5dba25bf
Arisha598 <arishachaudhary56@gmail.com>
2023-12-24 12:27:30 +0530

    Day1

---
+++ 1st.js
@@ -0,0 +1,13 @@
+//lets  constant
+const accountId = 1234
+let accountName ="Arisha"
+var accountNo ="12346"
+accountCity = "Hapur"
+accountName="Shifa"
+accountNo="22222"
+accountCity="mumbai"
+//it can print undfined instead of garbage value
+let accountHolder;
+//console.table is used to print the value in tabular form.
+console.table([accountName,accountNo,accountCity,accountHolder]);
+console.log(accountId);
