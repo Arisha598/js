ac410acad4b0e7b4d0b98d8928be886db0aa00f2
Arisha598 <arishachaudhary56@gmail.com>
2023-12-24 13:23:49 +0530

    Js-Tutorial

---
+++ Day1/2nd.js
@@ -0,0 +1,25 @@
+//Datatype
+
+"use strict"; //means all js code treated as a newer version
+
+// alert(3+3)---> we are using node js ,not browser
+
+//DataType in js
+/*
+1.number --> range(2 ki power 53)
+2.bigint --> it has very large no
+3.string -->"";
+4.boolean --> true/false;
+5.undefined --> the value is not defined
+6.null --> it means the string or variable is empty
+*/
+let a = 23
+let b="arisha"
+let c = true
+let d=null // object
+let e;
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
