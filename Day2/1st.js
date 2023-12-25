119744c754c4fdf068873c2176c7ee514de6f951
Arisha598 <arishachaudhary56@gmail.com>
2023-12-25 18:27:08 +0530

    Day2

---
+++ Day2/1st.js
@@ -0,0 +1,8 @@
+//Conversion i js
+let score = "33abc"
+console.log(typeof score);
+
+// by conversion :- convert one datatype to another
+let value = Number(score)
+console.log(typeof value);
+console.log(value) //:- it give nan(not a number);
---
+++ Day2/2nd.js
@@ -0,0 +1,18 @@
+//conversion into boolean
+
+let LoggedIn = 1
+let Bool = Boolean(LoggedIn )
+console.log(typeof Bool);
+console.log(Bool)
+
+//Taking an empty string
+let LoggedInto = "" // its give false
+let Bools = Boolean(LoggedInto )
+console.log(typeof Bools);
+console.log(Bools)
+
+//Taking an string
+let Logged = "Arisha" //:- its gives True
+let Boole = Boolean(Logged )
+console.log(typeof Boole);
+console.log(Boole)
