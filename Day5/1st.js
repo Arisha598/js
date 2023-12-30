15399ab96b73e03cf74318e6c6257da4a96ebd0d
Arisha598 <arishachaudhary56@gmail.com>
2023-12-30 22:46:50 +0530

    Day5

---
+++ Day5/1st.js
@@ -0,0 +1,15 @@
+//datatype in js:-
+//primitive (7-types)
+/*
+string , number , boolean , null , undefined, symbol,bigint */
+//non-Primitive
+/* object , function , array */
+
+let score = 123
+let scorevalue = 1.11
+let name = "arisha"
+
+// Symbol used:-
+let id=Symbol('12')
+let idnext = Symbol('12')
+console.log(id == idnext);
---
+++ Day5/2nd.js
@@ -0,0 +1,18 @@
+//array
+let value = ["arisha" , "shifa" , "azmee" , "adnan"]
+console.log(value)
+
+
+// objects
+let obj = {
+    name:"arisha",
+    age :20,
+}
+console.log(obj)
+
+
+
+//Function
+const myfunction = function(){
+    console.log("hello world")
+}
