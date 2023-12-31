8e27db6acc1431f0c00604b9655b84f316dc8151
Arisha598 <arishachaudhary56@gmail.com>
2023-12-31 20:57:00 +0530

    Day6

---
+++ Day6/1st.js
@@ -0,0 +1,13 @@
+//Memory
+/*
+There are two type of memory
+1.Stack(primitive datatype) -->create copy
+2.Heap(non-primitive datatype) -->Reference is passed
+*/
+
+
+let myYouTubename = "arishachaudhary"
+let anothername =  myYouTubename
+anothername = "Thegreatcoder"
+console.log(myYouTubename);
+console.log(anothername);
---
+++ Day6/2nd.js
@@ -0,0 +1,11 @@
+//non-primitive datatype
+
+let userone = {
+    email:"user@113gmail.com",
+    mobileno:12356,
+}
+let usertwo = userone 
+
+usertwo.email ="arisha123@gmail.com"
+console.log(userone);
+console.log(usertwo) 
